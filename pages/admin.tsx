91687bf7e45eeaef9d7c14b588a6e5b77d25ebf6
matthewlewandowski93 <matthew.lewandowski93@gmail.com>
2024-05-11 13:37:55 +1000

    admin block

--- pages/admin.tsx
+++ pages/admin.tsx
@@ -1,8 +1,48 @@
 import {ImageUploader} from "gmaker/src/components/admin/image-uploader";
+import {useAuthContext} from "gmaker/src/auth/auth-provider";
+import {CircularProgress, Stack, Typography} from "@mui/material";
+import { useRouter } from "next/router";
+
+const FullPageLoader = () => {
+
+    return (
+        <Stack
+            spacing={2}
+            sx={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                height: "80vh",
+            }}>
+            <CircularProgress size={100} thickness={10}/>
+            <Typography>
+                Loading...
+            </Typography>
+        </Stack>
+    )
+}
+
+
 
 const Admin = () => {
-
     return <ImageUploader/>
 }
 
-export default Admin
+
+const UserLoader = () => {
+    const router = useRouter()
+    const {user} = useAuthContext()
+
+    if (!user) {
+        return <FullPageLoader/>
+    }
+
+    if (!user.admin) {
+        router.replace("/")
+        return <FullPageLoader/>
+    }
+
+    return <Admin/>
+}
+
+export default UserLoader
